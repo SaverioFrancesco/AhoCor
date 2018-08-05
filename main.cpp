03fe8071c88bbd2ddee2be8090538e231bb568a2
Francesco S. Comisso <mitramdhir@gmail.com>
2018-08-05 18:27:32 +0200

    working commit

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#include <queue>
+#include <deque>
 
 using namespace std;
+
+
+int idGen = 0;
 
 class KTNode {
 
 public:
-    explicit KTNode(char c) {
+
+    int id;
+
+    KTNode(char c, int _id) {
         this->c = c;
         failure = nullptr;
+        id = _id;
     }
 
     ~KTNode() = default;
+
+    void setId(int _id) { id = _id; };
+
 
     void proliferate(string t) {
         if (t.empty()) return;
@@ -23,7 +33,7 @@ public:
                 return;
             }
         }
-        auto aux = new KTNode(t.front());
+        auto aux = new KTNode(t.front(), idGen++);
         aux->proliferate(t.substr(1));
         sons.push_back(aux);
     }
@@ -42,36 +52,49 @@ public:
         this->failure = fl;
     }
 
+    //void printOut(){ cout << id <<", faliure=" << (int)failure <<", content=" << getchar() << endl;}
+
+
     void computeFL() {
+
+        cout << "start commpute fl on "<< this -> id << endl;
         set<KTNode *> nodes;
         for (auto &son:sons)
             nodes.insert(son);
-        int x=0;
+        int x = 0;
+
+        cout<<"sons:" <<endl;
+        for (auto &son:nodes) {cout << son->id <<endl; };
 
         KTNode *currentFL = failure;
 
         while (!nodes.empty()) {
             set<KTNode *> tbd;
             for (auto &node : nodes) {
-                if(currentFL==currentFL->getFailure() && x>0){ // sono alla radice
+                if (currentFL == currentFL->getFailure() && x > 0) { // sono alla radice
                     node->setFailure(currentFL);
                     //nodes.erase(node);
                     tbd.insert(node);
                     continue;
                 }
                 for (auto &sof:currentFL->getSons()) {
-                    if (sof->getChar() == node->getChar()) {
+                    if (sof->getChar() == node->getChar() && node->id != sof->id) {
                         node->setFailure(sof);
                         //nodes.erase(node);
                         tbd.insert(node);
                     }
                 }
             }
-            for(auto &el:tbd){
+            for (auto &el:tbd) {
+
+                cout <<"delete "<< el->id <<endl;
                 nodes.erase(el);
             }
             currentFL = currentFL->getFailure();
             x++;
+
+            for (auto &son:nodes) {cout <<"alla fine"<< son->id <<endl << "........"<<endl; };
+
         }
     }
 
@@ -84,8 +107,9 @@ private:
 class KeywordTree {
 
 public:
+
     KeywordTree() {
-        root = new KTNode('$');
+        root = new KTNode('$', idGen++);
         root->setFailure(root);
     };
 
@@ -104,34 +128,60 @@ public:
     }
 
     void computeFL() {
-        queue<KTNode *> q;
+        deque<KTNode *> q;
 
-        q.push(root);
+        q.push_back(root);
+
+
+        for (auto &n : q) {
+            cout << n->getChar() << "," << n->id << endl;
+        }
 
         while (!q.empty()) {
             KTNode *cur = q.front();
             cur->computeFL();
             for (auto &son:cur->getSons()) {
-                q.push(son);
+                q.push_back(son);
             }
-            q.pop();
+            q.pop_front();
+
+            for (auto &n : q) {
+                cout << n->getChar() << "," << n->id << endl;
+
+            }
+            cout << "---" << endl;
         }
+
     }
 
     void printFL() {
-        queue<KTNode *> q;
+        deque<KTNode *> q;
 
-        q.push(root);
+        q.push_back(root);
 
         while (!q.empty()) {
             KTNode *cur = q.front();
             KTNode *fl = cur->getFailure();
-            cout << "FL di " << cur->getChar() << " è " << fl->getChar() << endl;
+            cout << "FL di " << cur->getChar() << "," << cur->id << " è " << fl->getChar() << "," << fl->id << endl;
             for (auto &son:cur->getSons()) {
-                q.push(son);
+                q.push_back(son);
             }
-            q.pop();
+            q.pop_front();
         }
+
+    }
+
+    void printPaths(KTNode *n) {
+
+        cout << n->getChar() << "," << n->id << endl;
+
+        cout << n->getFailure()->getChar() << "," << n->getFailure()->id << endl;
+        for (auto &son : n->getSons()) {
+            cout << "  -sons->" << son->getChar() << "," << son->id << endl;
+        }
+
+        for (auto &son : n->getSons()) { printPaths(son); }
+
 
     }
 
@@ -169,10 +219,14 @@ int main() {
     //k->insert(inp);
     k->insert("abc");
     k->insert("cab");
+    k->insert("cacao");
+    k->insert("cbcao");
 
     k->computeFL();
 
     k->printFL();
+
+    k->printPaths(k->getRoot());
 
     string a;
     serialize(k->getRoot(), &a);
